0d9343c1f3b75e4489119141a865e439ba6b57dc
William Tomas <william.tomas@mobiapps.fr>
2020-10-09 14:26:29 +0200

    Initial Commit

---
+++ Emojibook/EmojibookApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojibookApp.swift
+//  Emojibook
+//
+//  Created by William Tomas on 09/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct EmojibookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
